d02fc833eff40dffcac4a0368ae08e432e7dbf1d
jpfloau <jpfloau@gmail.com>
2020-06-28 20:01:03 -0400

    Created Employee class and test employee implementor class.

---
+++ src/com/Implementors/EmployeeImpl.java
@@ -0,0 +1,67 @@
+package com.Implementors;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import com.Models.Employee;
+
+public class EmployeeImpl {
+	
+	private ArrayList employeeList;
+	
+	public EmployeeImpl() {
+		this.employeeList = new ArrayList<Employee>();
+	}
+
+	public static void main(String[] args) {
+		EmployeeImpl em = new EmployeeImpl();
+		Scanner sc = new Scanner(System.in);
+		boolean sessionActive = true;
+		while(sessionActive) {
+			System.out.println("Please enter a first name for your employee");
+			String firstname = sc.next();
+			System.out.println("Please enter a last name for your employee");
+			String lastname = sc.next();
+			Employee temp = new Employee(firstname, lastname);
+			em.addEmployee(temp);
+			System.out.println("These are the employees so far");
+			System.out.println(em.allEmployee());
+			
+			System.out.println("Would you like to continue? Y or N");
+			String ans = sc.next();
+			while(!em.validateAns(ans)) {
+				System.out.println("Please enter valid answer");
+				ans = sc.next();
+			}
+			while(em.validateAns(ans)) {
+				if(ans.equalsIgnoreCase("Y")) {
+					break;
+				}else {
+					sessionActive = false;
+					System.out.println("Ok thank you");
+					break;
+				}
+			}
+		}
+	}
+	
+	public boolean validateAns(String ans) {
+		if(ans.equalsIgnoreCase("Y") || ans.equalsIgnoreCase("N")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void addEmployee(Employee e) {
+		this.employeeList.add(e);
+	}
+	
+	public String allEmployee() {
+		String allEmployees = "";
+		for(int i = 0; i < this.employeeList.size(); i++) {
+			allEmployees += this.employeeList.get(i).toString() + " ";
+		}
+		return allEmployees;
+	}
+}
---
+++ src/com/Models/Employee.java
@@ -0,0 +1,109 @@
+package com.Models;
+
+import java.util.Date;
+
+public class Employee {
+
+//	Attributes
+	private String firstName;
+	private String lastName;
+	private Date DoB;
+	private int Employee_ID;
+	
+	//	Employee: Full-time, Part-time, forloughed, laid-off 
+	private String Employee_status;
+
+	public Employee(String firstName, String lastName, Date doB, int employee_ID, String employee_status) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.DoB = doB;
+		this.Employee_ID = employee_ID;
+		this.Employee_status = employee_status;
+	}
+	
+	public Employee(String firstName, String lastName, Date doB, String employee_status) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.DoB = doB;
+		this.Employee_status = employee_status;
+	}
+	
+	public Employee(String firstName, String lastName, Date doB) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.DoB = doB;
+	}
+     
+	public Employee(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	public Employee() {
+		super();
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public Date getDoB() {
+		return DoB;
+	}
+
+	public void setDoB(Date doB) {
+		DoB = doB;
+	}
+
+	public int getEmployee_ID() {
+		return Employee_ID;
+	}
+
+	public void setEmployee_ID(int employee_ID) {
+		Employee_ID = employee_ID;
+	}
+
+	public String getEmployee_status() {
+		return Employee_status;
+	}
+
+	public void setEmployee_status(String employee_status) {
+		Employee_status = employee_status;
+	}
+
+	@Override
+	public String toString() {
+		return  firstName + " " + lastName;
+	}
+	
+	
+}
+
+
+//Object
+/*
+*
+*		Class name
+*			Parameters/Attributes
+*
+*			Constructor (Not necessary)
+*
+*				Method() usually called the same name as the class	
+*
+*				Initializes the object with required data		
+*
+*			Methods
+*
+*/
