b0847148580b1385a49a3e93e07e295eaffc9472
jwith <jwith@DESKTOP-B382AD1>
2020-06-29 19:27:11 -0400

    Added Employer model

---
+++ src/com/Models/Employer.java
@@ -0,0 +1,35 @@
+package com.Models;
+
+import java.util.Date;
+
+public class Employer {
+	
+//	Attributes
+	private String businessName;
+	private String phoneNumber;
+	private String Address;
+	private String hoursofOperation;
+	private Date datesClosed;
+	private String managerName;
+	private int numberofEmployees;
+	
+	public Employer(String businessName, String phoneNumber, String Address, String hoursofOperation, Date datesClosed, String mangerName, String number of Employees) {
+		super();
+		this.businessName = businessName;
+		this.phoneNumber = phoneNumber;
+		this.Address = Address;
+		this.hoursofOperation = hoursofOperation;
+		this.datesClosed = datesClosed;
+		this.managerName = managerName;
+		this.numberofEmployees = numberofEmployees;
+		
+	}
+	
+//	Constructor
+	
+//	Methods (Getter and Setter, and anything else need i.e toString method)
+	
+	
+//	For easy and fast creation of constructor and methods you can right click -> source -> Constructor/Getter and setter
+
+}
