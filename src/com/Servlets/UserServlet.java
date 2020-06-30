de7993b75c33575dce8d3956ff16db3694a257e7
jpfloau <jpfloau@gmail.com>
2020-06-30 11:58:00 -0400

    front end updates

--- src/com/Servlets/UserServlet.java
+++ src/com/Servlets/UserServlet.java
@@ -1,17 +1,13 @@
 package com.Servlets;
-
 import java.io.IOException;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.Models.User;
 import com.controllers.UserController;
-
 /**
  * Servlet implementation class UserServlet
  */
