8a5b60e312a35223b954858e0280668e3b38b3a9
jwith <jwith@DESKTOP-B382AD1>
2020-07-01 01:37:21 -0400

    Added getter and setter for Employer having trouble with lmpl

---
+++ src/com/Implementors/EmployerImpl.java
@@ -0,0 +1,7 @@
+package com.Implementors;
+
+public class EmployerImpl {
+	
+	
+
+}
--- src/com/Models/Employer.java
+++ src/com/Models/Employer.java
@@ -3,9 +3,12 @@ package com.Models;
 import java.util.Date;
 
 public class Employer {
-	
+	//asdojfnalsijfnasdlkijfbnasldkjgfbasdlkjb
+//	
+//	
 //	Attributes
 	private String businessName;
+//	variable for phone number of the employer
 	private String phoneNumber;
 	private String Address;
 	private String hoursofOperation;
@@ -13,7 +16,7 @@ public class Employer {
 	private String managerName;
 	private int numberofEmployees;
 	
-	public Employer(String businessName, String phoneNumber, String Address, String hoursofOperation, Date datesClosed, String mangerName, String number of Employees) {
+	public Employer(String businessName, String phoneNumber, String Address, String hoursofOperation, Date datesClosed, String mangerName, int numberofEmployees) {
 		super();
 		this.businessName = businessName;
 		this.phoneNumber = phoneNumber;
@@ -21,15 +24,16 @@ public class Employer {
 		this.hoursofOperation = hoursofOperation;
 		this.datesClosed = datesClosed;
 		this.managerName = managerName;
+//		Getting  type mistamatch error, not sure why. 
 		this.numberofEmployees = numberofEmployees;
 		
 	}
-	
+
 //	Constructor
 	
 //	Methods (Getter and Setter, and anything else need i.e toString method)
 	
-	
+
 //	For easy and fast creation of constructor and methods you can right click -> source -> Constructor/Getter and setter
 
 }
