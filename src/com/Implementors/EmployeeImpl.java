763858c26794c471c577e6f0d3cd372731c91f3e
jpfloau <jpfloau@gmail.com>
2020-07-01 14:34:41 -0400

    classpath changes to fix junit test error and commented out main method in employeeimpl

--- src/com/Implementors/EmployeeImpl.java
+++ src/com/Implementors/EmployeeImpl.java
@@ -14,37 +14,37 @@ public class EmployeeImpl {
 		this.employeeList = new ArrayList<Employee>();
 	}
 
-	public static void main(String[] args) {
-		EmployeeImpl em = new EmployeeImpl();
-		Scanner sc = new Scanner(System.in);
-		boolean sessionActive = true;
-		while(sessionActive) {
-			System.out.println("Please enter a first name for your employee");
-			String firstname = sc.next();
-			System.out.println("Please enter a last name for your employee");
-			String lastname = sc.next();
-			Employee temp = new Employee(firstname, lastname);
-			em.addEmployee(temp);
-			System.out.println("These are the employees so far");
-			em.allEmployee();
-			System.out.println("Would you like to continue? Y or N");
-			String ans = sc.next();
-			while(!em.validateAns(ans)) {
-				System.out.println("Please enter valid answer");
-				ans = sc.next();
-			}
-			while(em.validateAns(ans)) {
-				if(ans.equalsIgnoreCase("Y")) {
-					break;
-				}else {
-					sessionActive = false;
-					System.out.println("Ok thank you");
-					break;
-				}
-			}
-			sc.close();
-		}
-	}
+//	public static void main(String[] args) {
+//		EmployeeImpl em = new EmployeeImpl();
+//		Scanner sc = new Scanner(System.in);
+//		boolean sessionActive = true;
+//		while(sessionActive) {
+//			System.out.println("Please enter a first name for your employee");
+//			String firstname = sc.next();
+//			System.out.println("Please enter a last name for your employee");
+//			String lastname = sc.next();
+//			Employee temp = new Employee(firstname, lastname);
+//			em.addEmployee(temp);
+//			System.out.println("These are the employees so far");
+//			em.allEmployee();
+//			System.out.println("Would you like to continue? Y or N");
+//			String ans = sc.next();
+//			while(!em.validateAns(ans)) {
+//				System.out.println("Please enter valid answer");
+//				ans = sc.next();
+//			}
+//			while(em.validateAns(ans)) {
+//				if(ans.equalsIgnoreCase("Y")) {
+//					break;
+//				}else {
+//					sessionActive = false;
+//					System.out.println("Ok thank you");
+//					break;
+//				}
+//			}
+//			sc.close();
+//		}
+//	}
 	
 	public boolean validateAns(String ans) {
 		if(ans.equalsIgnoreCase("Y") || ans.equalsIgnoreCase("N")) {
